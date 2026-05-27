81367b3a672c5f590dfc09092fb0f54224e76aa3
Stuti <stuti.zingade@gmail.com>
2026-05-28 00:01:02 +0530

    draft14

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import Product from '../models/productModel.js';
-
+import expressAsyncHandler from 'express-async-handler';
 const productRouter = express.Router();
 productRouter.get('/', async (req, res) => {
   const products = await Product.find({});
