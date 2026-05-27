71afb3655b707a9e58de41e58756e370ebb4fa99
Stuti <stuti.zingade@gmail.com>
2026-05-27 23:22:32 +0530

    draft10

--- backend/server.js
+++ backend/server.js
@@ -1,43 +1,62 @@
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
-import productModel from './models/productModel.js';
 import userRouter from './routes/userRoutes.js';
 import orderRouter from './routes/orderRoutes.js';
-dotenv.config({ path: 'C:/Users/stuti/Downloads/app/backend/.env' });
-console.log(process.cwd());
+
+dotenv.config();
+
+const app = express();
+
 mongoose
   .connect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
-  });
-const app = express();
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((error) => console.error('MongoDB connection error:', error));
+
+const allowedOrigins = [
+  'https://amazonclone-frontend-t7t9.onrender.com',
+  'http://localhost:3000', // for local testing
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin (like Postman)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.get('/api/keys/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
+
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
 
-const _dirname = path.resolve();
-app.use(express.static(path.join(_dirname, '/frontend/build')));
-app.use((req, res) => {
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'));
-});
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
 const port = process.env.PORT || 5000;
+
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server running on port ${port}`);
 });
